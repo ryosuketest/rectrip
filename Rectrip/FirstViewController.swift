551bb7ad5203783b61b9b98f04daf36880e2ce9d
Ryosuke Kawamura <RyosukeKawamura@Ryosuke-Kawamura.local>
2015-05-25 18:34:04 +0900

    Initial Commit

---
+++ Rectrip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Rectrip
+//
+//  Created by Ryosuke Kawamura on 2015/05/25.
+//  Copyright (c) 2015年 informationer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
